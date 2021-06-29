7f954cb76f42192ec84ac255ae31173d5cb075ac
am720 <86016463+am720@users.noreply.github.com>
2021-06-29 11:57:21 -0700

    Update gladysSatellite.py

--- gladysSatellite.py
+++ gladysSatellite.py
@@ -3,3 +3,30 @@
      Module: gladysSatellite
      Description: This module does satellite stuff
 """
+def gpsAverage(x, y):
+"""
+document your function definition here. what does it do?
+"""
+"""
+delete the remaining code *in this function* and replace it withyour own code. add more code to do what the assignment asks of you.
+"""
+
+value = satellite.gpsValue(5, 6, "altitude")
+
+average = value / 2
+
+return average
+
+def distance(current, destination):
+
+""" 
+document your function definition here. what does it do?
+"""
+
+"""
+delete the remaining code *in this function* and replace it withyour own code. add more code to do what the assignment asks of you.
+"""
+
+distance = gpsAverage(3, 4)
+
+return distance
